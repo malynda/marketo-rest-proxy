680d1863cd25f488f8e94862cbfad76da2fb5a04
Malynda Chizek Frouard <malynda@spantree.net>
2014-06-11 15:05:35 -0500

    re-added sample files

---
+++ sample/syncLead.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var request   = require('request');
+var CryptoJS  = require('crypto');
+var moment    = require('moment');
+var config    = require(process.env.HOME + '/.marketo-api/config');
+
+function hashConvert(somestring) {
+  return CryptoJS.createHmac('sha1',config.secretKey).update(somestring).digest('hex');
+}
+
+var timestamp = moment().utc().format("YYYY-MM-DDTHH:mm:ssZ");
+
+request({
+  uri: 'http://localhost:3000/syncLead',
+  method: 'POST',
+  json: {
+    header: {
+      userId: config.userID,
+      requestSignature: hashConvert('' + timestamp + config.userID),
+      requestTimestamp: timestamp
+    },
+    leadRecord: {
+      Email: 'titus@operus.net',
+      FirstName: 'Titus',
+      LastName: 'Operus'
+    }
+  }}, function (err, response, body) {
+    if(err){
+      return console.error('ERROR: ', err);
+    } 
+    console.log('body: ', body);
+
+  }
+);
