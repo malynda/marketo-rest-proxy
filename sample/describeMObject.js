cc321a49f38c58a44edbef1626fc18eba3c68560
Malynda Chizek Frouard <malynda@spantree.net>
2014-06-11 15:07:51 -0500

    whitespace

--- sample/describeMObject.js
+++ sample/describeMObject.js
@@ -1,5 +1,5 @@
 'use strict';
- 
+
 var request   = require('request');
 var CryptoJS  = require('crypto');
 var moment    = require('moment');
@@ -8,9 +8,9 @@ var config    = require(process.env.HOME + '/.marketo-api/config');
 function hashConvert(somestring) {
   return CryptoJS.createHmac('sha1',config.secretKey).update(somestring).digest('hex');
 }
- 
+
 var timestamp = moment().utc().format("YYYY-MM-DDTHH:mm:ssZ");
- 
+
 request({
   uri: 'http://localhost:3000/describeLeadRecord',
   method: 'POST',
